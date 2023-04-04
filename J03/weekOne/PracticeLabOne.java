31c70217c5088d3c9c7f5efea443d1159149cfd7
Kee <shikee.maloney@codeimmersives.com>
2023-04-04 15:05:22 -0400

    all

--- J03/weekOne/PracticeLabOne.java
+++ J03/weekOne/PracticeLabOne.java
@@ -1,44 +1,53 @@
 import java.util.Arrays;
 
 public class PracticeLabOne {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    max3(23, 345, 43);
-    min3(4, 30, 46);
-    median(123, 54, 76);
-   }
-   public static void max3(int numOne, int numTwo, int numThree) {
-
-    int[]  MyArr = {numOne,numTwo,numThree};
-
-    int Max = MyArr[0];
-
-    for(int i = 0;i< MyArr.length; i++){
-        if(MyArr[i]>Max){
-            Max=MyArr[i];
-        }
+        Max3(23, 345, 43);
+        Min3(4, 30, 46);
+        Median(123, 54, 76);
+        XOR(true, false);
     }
-    System.out.println(Max);
-    
-   }
-   public static void min3(int numOne, int numTwo, int numThree) {
 
-    int[]  MyArr = {numOne,numTwo,numThree};
+    public static void Max3(int numOne, int numTwo, int numThree) {
 
-    int Min = MyArr[0];
+        int[] MyArr = { numOne, numTwo, numThree };
 
-    for(int i = 0;i< MyArr.length; i++){
-        if(MyArr[i]<Min){
-            Min=MyArr[i];
+        int Max = MyArr[0];
+
+        for (int i = 0; i < MyArr.length; i++) {
+            if (MyArr[i] > Max) {
+                Max = MyArr[i];
+            }
         }
-    }
-    System.out.println(Min);
-    
-   }
+        System.out.println(Max);
 
-   public static void median(int numOne, int numTwo, int numThree) {
-    int[]  MyArr = {numOne,numTwo,numThree};
-    Arrays.sort(MyArr);
-    System.out.println(MyArr[1]);
-   }
+    }
+
+    public static void Min3(int numOne, int numTwo, int numThree) {
+
+        int[] MyArr = { numOne, numTwo, numThree };
+
+        int Min = MyArr[0];
+
+        for (int i = 0; i < MyArr.length; i++) {
+            if (MyArr[i] < Min) {
+                Min = MyArr[i];
+            }
+        }
+        System.out.println(Min);
+
+    }
+
+    public static void Median(int numOne, int numTwo, int numThree) {
+        int[] MyArr = { numOne, numTwo, numThree };
+        Arrays.sort(MyArr);
+        System.out.println(MyArr[1]);
+    }
+
+    public static void XOR(boolean boolOne, boolean boolTwo) {
+        System.out.println(boolOne != boolTwo);
+    
+        }
+
 }
