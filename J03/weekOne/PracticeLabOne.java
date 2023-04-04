b7d2c0cc6dfdca7b40af42352180b70754abd4a5
Kee <shikee.maloney@codeimmersives.com>
2023-04-04 14:31:32 -0400

    all

--- J03/weekOne/PracticeLabOne.java
+++ J03/weekOne/PracticeLabOne.java
@@ -1,3 +1,21 @@
 public class PracticeLabOne {
+   public static void main(String[] args) {
+
+    max3(23, 345, 43);
     
+   }
+   public static void max3(int numOne, int numTwo, int numThree) {
+
+    int[]  MyArr = {numOne,numTwo,numThree};
+
+    int Max = MyArr[0];
+
+    for(int i = 0;i< MyArr.length; i++){
+        if(MyArr[i]>Max){
+            Max=MyArr[i];
+        }
+    }
+    System.out.println(Max);
+    
+   }
 }
