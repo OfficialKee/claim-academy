a84c14449e39e0057dff72be124581bbc220a20d
Kee <shikee.maloney@codeimmersives.com>
2023-04-07 13:34:16 -0400

    all

--- J03/weekOne/PracticeLabThree.java
+++ J03/weekOne/PracticeLabThree.java
@@ -1,7 +1,8 @@
 public class PracticeLabThree {
     public static void main(String[] args) {
-        RemoveCharacter("Average`", 'a');
+        RemoveCharacter("Average", 'a');
         RemoveDuplicate("mississippi");
+        HowManyConsVowels("aaaaaaaabb");
     }
 
     public static void RemoveCharacter(String word, char RemChar) {
@@ -12,10 +13,9 @@ public class PracticeLabThree {
         String newStr = "";
         // began looping through the character array created above
         for (int i = 0; i < c.length; i++) {
-            if (c[i] != ((int) RemChar))
+            if (c[i] != RemChar)
                 newStr += c[i];
         }
-
         System.out.println(newStr);
     }
 
@@ -24,10 +24,40 @@ public class PracticeLabThree {
         String checkerString = "";
         for (int index = 0; index < c.length; index++) {
             if (!checkerString.contains("" + c[index] + "")) {
-                checkerString += c[index];  
+                checkerString += c[index];
             }
         }
         System.out.println(checkerString);
     }
 
+    public static void HowManyConsVowels(String input) {
+
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x',
+                'y', 'z' };
+        char[] word = input.toLowerCase().toCharArray();
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (int i = 0; i < word.length; i++) {
+            for (int j = 0; j < consonants.length; j++) {
+                if (word[i] == consonants[j]) {
+                    consonantCount++;
+                }
+
+            }
+        } 
+        for (int i = 0; i < word.length; i++) {
+            for (int j = 0; j < vowels.length; j++) {
+                if (word[i] == vowels[j]) {
+                    vowelCount++;
+                }
+
+            }
+
+        }
+        System.out.println("total consonant: "+consonantCount +" and totals vowels " + vowelCount);
+
+    }
 }
