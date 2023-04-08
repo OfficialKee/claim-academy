48e7ba1176dda3f5442a03ee6995ae3fa6fa869c
Kee <shikee.maloney@codeimmersives.com>
2023-04-08 16:47:34 -0400

    all

--- J03/weekTwo/Car.java
+++ J03/weekTwo/Car.java
@@ -1,40 +1,56 @@
 public class Car {
 
-    // properties for instance variables
-    private String make;
-    private String model;
-    private int year;
-    private double speed;
-   
-    //constructor
-    public Car(String make, String model, int year){
+    // properties or instance variables
+    public String make;
+    public String model;
+    public String color;
+    public int year;
+    public double speed;
+    public boolean isRunning;
+
+    // constructor
+    public Car(String make, String model, String color, int year) {
         this.make = make;
         this.model = model;
         this.year = year;
+        this.color = color;
+        this.isRunning = false;
     }
- // Methods..Functions...behaviors
+    // Methods..Functions...behaviors
 
- //method to increase the classes speed
-    public void accelerate(double incAmt){
+    // method to increase the classes speed
+
+    public void start() {
+        if (isRunning) {
+            isRunning = true;
+            System.out.println("The " + color + " " + make + " " + model + " has started");
+        } else {
+            isRunning = false;
+            System.out.println("The " + color + " " + make + " " + model + " is already stopped");
+        }
+    }
+
+    public void stop() {
+        if (isRunning) {
+            isRunning = false;
+            System.out.println("The " + color + " " + make + " " + model + " has stopped");
+        } else {
+            System.out.println("The " + color + " " + make + " " + model + " is already stopped");
+        }
+    }
+
+    public void accelerate(double incAmt) {
         this.speed += incAmt;
 
     }
 
-    public void brake(double decAmt){
+    public void brake(double decAmt) {
         this.speed -= decAmt;
 
     }
-    
+
     public static void main(String[] args) {
-    
-    
-        
-
-        
-
-           
 
     }
-
 
 }
---
+++ J03/weekTwo/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.text.html.FormSubmitEvent;
+
+public class Main {
+    public static void main(String[] args) {
+            Car mustang = new Car("ford", "mustang", "blue", 2023);
+
+
+            System.out.println(mustang.make);
+
+            mustang.start();
+
+            mustang.accelerate(20);
+
+            System.out.println("The mustang is going "+mustang.speed+" mph");
+
+            mustang.brake(5);
+
+            System.out.println("The mustang is now going "+mustang.speed+" mph");
+
+    
+    }
+}
