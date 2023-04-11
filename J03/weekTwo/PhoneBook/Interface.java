e77d41dcdfbafb2b77feefe5b5ed2dabb2f70cd0
Kee <shikee.maloney@codeimmersives.com>
2023-04-11 17:08:10 -0400

    planning

--- J03/weekTwo/PhoneBook/Interface.java
+++ J03/weekTwo/PhoneBook/Interface.java
@@ -1,5 +1,39 @@
 public class Interface {
     public Interface(){
-        
+
     }
 }
+
+// create a function that takes the person and the addess and add it to  an array of objects.
+/* [
+ * {name:
+ * address:},
+ * {name:
+ * address:},
+ * {name:
+ * address:},
+ * {name:
+ * address:},
+ * ]
+ * 
+ * 
+*/
+
+
+/*
+ * search by last name function
+ * 
+ * search by first name
+ * 
+ * search by full name
+ * 
+ * search by number
+ * 
+ * search by city
+ * 
+ * search by state
+ * 
+ * update by telephone number
+ * 
+ * delete by telephone number
+ */
