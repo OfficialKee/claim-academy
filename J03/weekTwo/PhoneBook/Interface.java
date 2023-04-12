2b91435772cf5d9ec8a1a8cdf3eef0fec07c88e4
Kee <shikee.maloney@codeimmersives.com>
2023-04-12 12:22:43 -0400

    all

--- J03/weekTwo/PhoneBook/Interface.java
+++ J03/weekTwo/PhoneBook/Interface.java
@@ -1,12 +1,51 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Interface {
-    public static void main(String[] args) {
-        
-    }
+   public static void main(String[] args) {
+    
+    Address myAddy = new Address(2943, "Spring Park rd", "Jacksonville", "Florida", 32207);
+
+    // System.out.println(myAddy.getStreetName());
+
+    Boolean running = true;
+
+        while(running){
+            welcomeUser();
+            Scanner scanner = new Scanner(System.in);
+            int value = scanner.nextInt();
+            if(value == 0){
+               running = false;
+            }
+            if(value == 1){
+
+            }
+            
+        }
+
+
+   }
+
+   public static void welcomeUser() {
+    System.out.println("\nWelcome to The PhoneBook");
+    System.out.println("\nChoose an option: ");
+    System.out.println(" \n------------------------");
+    System.out.println(" [0] Exit Phonebook App ");
+    System.out.println(" [1] Search contact ");
+    System.out.println(" [2] Add contact ");
+    System.out.println(" [3] Delete Contact ");
+    System.out.println(" [4] Update Contact ");
+    System.out.println(" ------------------------");
+    
+    
+    
+   }
+
+
+}
 
     
-} 
+
 
 // create a function that takes the person and the addess and add it to  an array of objects.
 /* [
